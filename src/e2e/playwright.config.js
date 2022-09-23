ac46c7e7d46413978db1362c3c6936cb4c3c38cb
edmgb <evangelos.dinopoulos@mgb.ch>
2022-09-23 08:59:32 +0200

    update

---
+++ src/e2e/playwright.config.js
@@ -0,0 +1,33 @@
+// @ts-check
+const { devices } = require('@playwright/test')
+
+const config = {
+  reporter: 'line',
+  use: {
+    baseURL: 'http://localhost:2200',
+    headless: true
+  },
+  projects: [
+    {
+      name: 'Mobile Android',
+      use: {
+        ...devices['Pixel 2']
+      }
+    },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    }
+  ],
+  snapshotDir: './tests/',
+  /* Run your local server before starting the tests */
+  webServer: {
+    command: 'npm run serve',
+    port: 4200,
+    reuseExistingServer: true
+  }
+}
+
+module.exports = config
